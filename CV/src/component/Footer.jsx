ae534b97d57429960115ad13327cfae7062c538e
maximepoujol1-dot <maximepoujol1@gmail.com>
2026-08-14 14:25:46 +0200

    Footer categorie

--- CV/src/component/Footer.jsx
+++ CV/src/component/Footer.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
         <nav>
-            <h6 className="footer-title">Services</h6>
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
+            <h6 className="footer-title">Liens</h6>
+            <a href="https://github.com/maximepoujol1-dot" className="link link-hover">Github</a>
+            <a href="https://www.linkedin.com/in/maxime-collette-poujol-40b0483a1/" className="link link-hover">LinkedIn</a>
+            <a href="" className="link link-hover">Instagram</a>
+            <a href="" className="link link-hover">FaceBook</a>
         </nav>
         <nav>
-            <h6 className="footer-title">Company</h6>
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
+            <h6 className="footer-title">Document</h6>
+            <a href="" className="link link-hover">CV</a>
+            <a href="" className="link link-hover">Portfolio</a>
+            <a href="" className="link link-hover">mentions légales</a>
+            <a href="" className="link link-hover">confidentialités</a>
         </nav>
         <nav>
-            <h6 className="footer-title">Legal</h6>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
+            <h6 className="footer-title">Navigation</h6>
+            <a href="" className="link link-hover"><Link to="/"> Accueuil</Link></a>
+            <a href="" className="link link-hover"><Link to="/portfolio"> Portfolio</Link></a>
+            <a href="" className="link link-hover"><Link to="/cv"> Cv </Link></a>
+            <a href="" className="link link-hover"><Link to="/contact"> Contact</Link></a>
         </nav>
         </footer>
   )
