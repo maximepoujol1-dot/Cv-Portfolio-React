d98babff020e97b73be2c8c78ff7ef83cf041206
maximepoujol1-dot <maximepoujol1@gmail.com>
2026-09-17 11:21:09 +0200

    Création d'un component hero avec début de la recréation de la homepage sur le modèle de l'ancienne

--- v2/src/component/Hero.tsx
+++ v2/src/component/Hero.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
 interface heroProps {
-
+ title: string
+ image: string  
+ style: string
+ children : React.ReactNode
 }
 
-const Hero = () => {
+const Hero = ({title,image,style,children}:heroProps) => {
   return (
-    <></>
+    <div className={"hero "+style}>
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          alt="Tailwind CSS hero component"
+          src={image}
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">{title}</h1>
+          {children}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- v2/src/view/HomePage.tsx
+++ v2/src/view/HomePage.tsx
@@ -1,9 +1,12 @@
-import Hero from "../component/Hero"
 
+import img1 from "../assets/test2.png";
+import img2 from "../assets/moi.png";
+import Card from "../component/Card";
+import Hero from "../component/Hero";
 const HomePage = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-center h-screen bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url(${img1})` }}>
+        <Card style={"bg-base-100 w-1/2 h-3/4 shadow-xl"} title={""} modal={false}><Hero title={"Qui suis je"} image={img2} style={""}> <p>text de présentation</p></Hero></Card>  
     </div>
   )
 }
